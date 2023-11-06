b2bf24a70ea29c5a0c871216079017a63871b85f
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-11-06 19:56:54 +0100

    Create action_type_service.go

---
+++ service/action_type_service.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	"errors"
+
+	"github.com/wissemmansouri/OpenIT.one-MessageBus/model"
+	"github.com/wissemmansouri/OpenIT.one-MessageBus/repository"
+)
+
+type ActionTypeService struct {
+	repository *repository.Repository
+}
+
+var (
+	ErrActionSourceIDNotFound = errors.New("event source id not found")
+	ErrActionNameNotFound     = errors.New("event name not found")
+)
+
+func (s *ActionTypeService) GetActionTypes() ([]model.ActionType, error) {
+	return (*s.repository).GetActionTypes()
+}
+
+func (s *ActionTypeService) RegisterActionType(actionType model.ActionType) (*model.ActionType, error) {
+	// TODO - ensure sourceID and name are URL safe
+
+	return (*s.repository).RegisterActionType(actionType)
+}
+
+func (s *ActionTypeService) GetActionTypesBySourceID(sourceID string) ([]model.ActionType, error) {
+	return (*s.repository).GetActionTypesBySourceID(sourceID)
+}
+
+func (s *ActionTypeService) GetActionType(sourceID string, name string) (*model.ActionType, error) {
+	return (*s.repository).GetActionType(sourceID, name)
+}
+
+func NewActionTypeService(repository *repository.Repository) *ActionTypeService {
+	return &ActionTypeService{
+		repository: repository,
+	}
+}
