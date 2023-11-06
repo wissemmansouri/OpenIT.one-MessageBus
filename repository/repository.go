7ee20b350a7b63b63d343b3489b3bb57597d68f3
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-11-06 18:10:02 +0100

    Create repository.go

---
+++ repository/repository.go
@@ -0,0 +1,17 @@
+package repository
+
+import "github.com/wissemmansouri/OpenIT.one-MessageBus/model"
+
+type Repository interface {
+	GetEventTypes() ([]model.EventType, error)
+	RegisterEventType(eventType model.EventType) (*model.EventType, error)
+	GetEventTypesBySourceID(sourceID string) ([]model.EventType, error)
+	GetEventType(sourceID string, name string) (*model.EventType, error)
+
+	GetActionTypes() ([]model.ActionType, error)
+	RegisterActionType(actionType model.ActionType) (*model.ActionType, error)
+	GetActionTypesBySourceID(sourceID string) ([]model.ActionType, error)
+	GetActionType(sourceID string, name string) (*model.ActionType, error)
+
+	Close()
+}
