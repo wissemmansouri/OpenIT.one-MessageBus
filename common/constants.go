653dffecd68e6aac35a78b89ff83713ece25da72
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-11-06 17:59:16 +0100

    Create constants.go

---
+++ common/constants.go
@@ -0,0 +1,9 @@
+package common
+
+const (
+	MessageBusVersion     = "0.4.4"
+	MessageBusServiceName = "message-bus"
+
+	MessageBusSourceID      = "message-bus"
+	MessageBusHeartbeatName = "message-bus:heartbeat"
+)
