3fc739f0d6ce331098957edf29a04abaf1fe0b53
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-11-06 18:06:49 +0100

    Create init.go

---
+++ config/init.go
@@ -0,0 +1,90 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/wissemmansouri/OpenIT.one-Common/utils/constants"
+	"github.com/wissemmansouri/OpenIT.one-MessageBus/common"
+	"github.com/wissemmansouri/OpenIT.one-MessageBus/model"
+	"gopkg.in/ini.v1"
+)
+
+var (
+	CommonInfo = &model.CommonModel{
+		RuntimePath: constants.DefaultRuntimePath,
+	}
+
+	AppInfo = &model.APPModel{
+		LogPath:     constants.DefaultLogPath,
+		LogSaveName: common.MessageBusServiceName,
+		LogFileExt:  "log",
+	}
+
+	Cfg            *ini.File
+	ConfigFilePath string
+)
+
+func InitSetup(config string, sample string) {
+	ConfigFilePath = MessageBusConfigFilePath
+	if len(config) > 0 {
+		ConfigFilePath = config
+	}
+
+	// create default config file if not exist
+	if _, err := os.Stat(ConfigFilePath); os.IsNotExist(err) {
+		fmt.Println("config file not exist, create it")
+		// create config file
+		file, err := os.Create(ConfigFilePath)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+
+		// write default config
+		_, err = file.WriteString(sample)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	var err error
+
+	Cfg, err = ini.Load(ConfigFilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	mapTo("common", CommonInfo)
+	mapTo("app", AppInfo)
+}
+
+func SaveSetup(config string) {
+	reflectFrom("common", CommonInfo)
+	reflectFrom("app", AppInfo)
+
+	configFilePath := MessageBusConfigFilePath
+	if len(config) > 0 {
+		configFilePath = config
+	}
+
+	if err := Cfg.SaveTo(configFilePath); err != nil {
+		log.Printf("error when saving to %s", configFilePath)
+		panic(err)
+	}
+}
+
+func mapTo(section string, v interface{}) {
+	err := Cfg.Section(section).MapTo(v)
+	if err != nil {
+		log.Fatalf("Cfg.MapTo %s err: %v", section, err)
+	}
+}
+
+func reflectFrom(section string, v interface{}) {
+	err := Cfg.Section(section).ReflectFrom(v)
+	if err != nil {
+		log.Fatalf("Cfg.ReflectFrom %s err: %v", section, err)
+	}
+}
