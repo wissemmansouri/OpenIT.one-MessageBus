85448a7011931bc663dc90b607a5b476215cb79b
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-11-08 16:37:28 +0100

    Create services.go

---
+++ service/services.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"context"
+	"errors"
+
+	"github.com/wissemmansouri/OpenIT.one-MessageBus/repository"
+)
+
+type Services struct {
+	EventTypeService *EventTypeService
+	EventServiceWS   *EventServiceWS
+
+	ActionTypeService *ActionTypeService
+	ActionServiceWS   *ActionServiceWS
+
+	SocketIOService *SocketIOService
+}
+
+var (
+	ErrInboundChannelNotFound     = errors.New("inbound channel not found")
+	ErrSubscriberChannelsNotFound = errors.New("subscriber channels not found")
+	ErrAlreadySubscribed          = errors.New("already subscribed")
+)
+
+func (s *Services) Start(ctx *context.Context) {
+	go s.EventServiceWS.Start(ctx)
+	go s.ActionServiceWS.Start(ctx)
+
+	go s.SocketIOService.Start(ctx)
+}
+
+func NewServices(repository *repository.Repository) Services {
+	eventTypeService := NewEventTypeService(repository)
+	actionTypeService := NewActionTypeService(repository)
+
+	return Services{
+		EventTypeService: eventTypeService,
+		EventServiceWS:   NewEventServiceWS(eventTypeService),
+		SocketIOService:  NewSocketIOService(),
+
+		ActionTypeService: actionTypeService,
+		ActionServiceWS:   NewActionServiceWS(actionTypeService),
+	}
+}
