f8544b849904ba23960ae7fedd3865c64cdec153
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-11-08 16:32:52 +0100

    Create event_type_service.go

---
+++ service/event_type_service.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	"errors"
+
+	"github.com/wissemmansouri/OpenIT.one-MessageBus/model"
+	"github.com/wissemmansouri/OpenIT.one-MessageBus/repository"
+)
+
+type EventTypeService struct {
+	repository *repository.Repository
+}
+
+var (
+	ErrEventSourceIDNotFound = errors.New("event source id not found")
+	ErrEventNameNotFound     = errors.New("event name not found")
+)
+
+func (s *EventTypeService) GetEventTypes() ([]model.EventType, error) {
+	return (*s.repository).GetEventTypes()
+}
+
+func (s *EventTypeService) RegisterEventType(eventType model.EventType) (*model.EventType, error) {
+	// TODO - ensure sourceID and name are URL safe
+
+	return (*s.repository).RegisterEventType(eventType)
+}
+
+func (s *EventTypeService) GetEventTypesBySourceID(sourceID string) ([]model.EventType, error) {
+	return (*s.repository).GetEventTypesBySourceID(sourceID)
+}
+
+func (s *EventTypeService) GetEventType(sourceID string, name string) (*model.EventType, error) {
+	return (*s.repository).GetEventType(sourceID, name)
+}
+
+func NewEventTypeService(repository *repository.Repository) *EventTypeService {
+	return &EventTypeService{
+		repository: repository,
+	}
+}
