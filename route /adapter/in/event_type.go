3fe0b0912b0752cc0fc6d70291b86c4f45543ba9
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-11-14 14:47:12 +0100

    Create event_type.go

---
+++ route /adapter/in/event_type.go
@@ -0,0 +1,27 @@
+package in
+
+import (
+	"github.com/wissemmansouri/OpenIT.one-MessageBus/codegen"
+	"github.com/wissemmansouri/OpenIT.one-MessageBus/model"
+)
+
+func EventAdapter(event codegen.Event) model.Event {
+	// properties := make([]model.Property, 0)
+	// for _, property := range  {
+	// 	properties = append(properties, PropertyAdapter(property))
+	// }
+
+	var timestamp int64
+	if event.Timestamp != nil {
+		timestamp = event.Timestamp.Unix()
+	}
+
+	return model.Event{
+		SourceID:   event.SourceID,
+		Name:       event.Name,
+		Properties: event.Properties,
+		UUID:       *event.Uuid,
+
+		Timestamp: timestamp,
+	}
+}
