38b12e6f491add83f21ef98f73340f27b2b0104c
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-11-14 14:53:20 +0100

    Create property.go

---
+++ route /adapter/out/property.go
@@ -0,0 +1,8 @@
+package out
+
+// func PropertyAdapter(property model.Property) codegen.Property {
+// 	return codegen.Property{
+// 		Name:  property.Name,
+// 		Value: property.Value,
+// 	}
+// }
