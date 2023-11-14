7e0ba2bf1b503806d4fed33cb187a8de1bd62d1d
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-11-14 14:51:07 +0100

    Create action_type.go

---
+++ route /adapter/out/action_type.go
@@ -0,0 +1,19 @@
+package out
+
+import (
+	"github.com/wissemmansouri/OpenIT.one-MessageBus/codegen"
+	"github.com/wissemmansouri/OpenIT.one-MessageBus/model"
+)
+
+func ActionTypeAdapter(actionType model.ActionType) codegen.ActionType {
+	propertyTypeList := make([]codegen.PropertyType, 0)
+	for _, propertyType := range actionType.PropertyTypeList {
+		propertyTypeList = append(propertyTypeList, PropertyTypeAdapter(propertyType))
+	}
+
+	return codegen.ActionType{
+		SourceID:         actionType.SourceID,
+		Name:             actionType.Name,
+		PropertyTypeList: propertyTypeList,
+	}
+}
